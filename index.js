1ca57cc39b6942fe1fbcc21b65c9b03cf65d0611
danygagnon <ddanygagnon@gmail.com>
2021-09-24 15:12:26 -0400

    feat(*): base game with functions
    the basic game with functionnal programming.

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-const events = ['click', 'contextmenu', 'dblclick', 'mouseenter']
+import { triggerScore } from './score.js';
+
+const events = ['click', 'contextmenu', 'dblclick', 'mouseenter'];
 
 const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 
-const score = $('#score')
-
 const createImage$ = (event) => $(`
-    <img src="./img/${event}.png" alt="${event}" />
-`)
-const animateBottom = (speed, fn) => ($elem) => $elem.animate({ bottom: 0 }, speed, fn)
-const randomPosition = ($elem) => $elem.css({ left: `${Math.random() * 100}%` })
-const randomArrayElement = (array) => array[Math.floor(Math.random() * array.length)];
+    <img src='./img/${event}.png?' alt='${event}' class='alive' />
+`);
 
-const triggerScore = (event) => ($elem) => $elem.on(event, () => {
-    score.trigger('score', -10)
-    return $elem.remove()
-});
+const animateBottom = ($elem) =>
+    $elem.animate(
+        { bottom: 0 }, 3000, () => eventDead($elem)
+    );
 
-score.on('score', (data, elem) => {
-    console.log(elem)
-})
+const randomPosition = ($elem) =>
+    $elem.css({ left: `${Math.random() * 100}vw` });
+
+const randomArrayElement = (array) =>
+    array[Math.floor(Math.random() * array.length)];
+
+const randomNumberInterval = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min)
+
+const eventAlive = ($elem) => $elem.toggleClass('alive') &&
+    $elem.remove() &&
+    triggerScore(10);
+
+const eventDead = ($elem) => $elem.hasClass('alive') &&
+    $elem.remove() &&
+    triggerScore(-20)
+
+const addEvent = (event) => ($elem) => $elem.on(
+    event, () => eventAlive($elem)
+);
+
+const appendToBody = ($elem) => $elem.appendTo('body');
+
+const createEventBlock = (event) =>
+    pipe(
+        createImage$,
+        appendToBody,
+        animateBottom,
+        randomPosition,
+        addEvent(event)
+    )(event);
+
+const createRandomEventBlock = () =>
+    pipe(randomArrayElement, createEventBlock)(events);
+
+const recursiveEventBlock = () => {
+    const randomNumberBlocks = randomNumberInterval(1, 3);
+    [...Array(randomNumberBlocks)].forEach(createRandomEventBlock)
+}
 
 $(document).ready(() => {
-    const event = randomArrayElement(events);
-    const $eventImage = createImage$(event)
-    $('body').append($eventImage);
-    pipe(
-        animateBottom(3000),
-        randomPosition,
-        triggerScore(event)
-    )($eventImage)
-})
-
+    setInterval(recursiveEventBlock, 2000)
+});
---
+++ score.js
@@ -0,0 +1,9 @@
+const score = $('#score');
+
+let scoreValue = 0;
+score.on('score', (_, elem) => {
+    scoreValue += elem;
+    score.text(scoreValue);
+});
+
+export const triggerScore = (scoreValue) => score.trigger('score', scoreValue);
